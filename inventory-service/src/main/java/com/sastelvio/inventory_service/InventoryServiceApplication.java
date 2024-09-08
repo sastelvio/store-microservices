bf5222e13c61ac5b52df9f51eedc6feaeecc491a
sastelvios <sast.serafim@gmail.com>
2024-09-08 05:07:56 +0200

    fix Discovery Server instance-id when same miscroservice has many inscances

--- inventory-service/src/main/java/com/sastelvio/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/sastelvio/inventory_service/InventoryServiceApplication.java
@@ -2,18 +2,33 @@ package com.sastelvio.inventory_service;
 
 import com.sastelvio.inventory_service.model.Inventory;
 import com.sastelvio.inventory_service.repository.InventoryRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class InventoryServiceApplication {
 
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Autowired
+	private EurekaInstanceConfigBean eurekaInstanceConfigBean;
+
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		eurekaInstanceConfigBean.setNonSecurePort(serverPort);
 	}
 
 	//CARREGAR DADOS AO INICIAR A APLICACAO
